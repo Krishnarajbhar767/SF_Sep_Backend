7433689eada38605fc88a044a0eb70188e19ffbb
Krishnarajbhar767 <bloggerkrishna767@gmail.com>
2025-09-27 12:35:24 +0530

    Removed MODULE Syntax

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import { createBlog, deleteBlog, getBlogById, getBlogs, getBlogsAdmin, updateBlog } from '../controller/blog.controller.js';
-import { isAdmin, isAuthenticated } from '../middleware/auth.middleware.js';
+const express = require('express')
+const { createBlog, deleteBlog, getBlogById, getBlogs, getBlogsAdmin, updateBlog } = require('../controller/blog.controller.js')
+const { isAdmin, isAuthenticated } = require('../middleware/auth.middleware.js')
 
-export const blogRouter = express.Router();
+const blogRouter = express.Router();
 
 blogRouter.post('/', isAuthenticated, isAdmin, createBlog);
 blogRouter.put('/:id', isAuthenticated, isAdmin, updateBlog);
@@ -12,3 +12,8 @@ blogRouter.get('/', isAuthenticated, isAdmin, getBlogsAdmin);
 // User Get Blogs 
 blogRouter.get('/all', getBlogs);
 blogRouter.get('/:id', getBlogById);
+
+
+module.exports = {
+    blogRouter
+}
